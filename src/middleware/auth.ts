511baf39e6a10e499ae01437f5487bcd3890f6c9
NikhilPersonal-Code <tusharwankhede863@gmail.com>
2026-01-03 01:29:00 +0530

    chore:doing for warming up a another account

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -23,6 +23,7 @@ export const authenticate = async (
 ) => {
   try {
     
+
     const authHeader = req.headers.authorization;
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
       return res.status(401).json({ error: "Missing authorization header" });
